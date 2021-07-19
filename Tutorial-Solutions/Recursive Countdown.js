ec4da7416b9a41ce2cd471def078dff93288fdda
Tsquare25 <tibebe1234t@gmail.com>
2021-07-19 19:58:56 +0100

    Final sub-project solution

--- Tutorial-Solutions/Recursive Countdown.js
+++ Tutorial-Solutions/Recursive Countdown.js
@@ -11,3 +11,31 @@ function countdown(n){
 }
 // Only change code above this line
 countdown(10)
+
+
+/*Continuing from the previous challenge, we provide you another opportunity 
+to create a recursive function to solve a problem.
+
+We have defined a function named rangeOfNumbers with two parameters.
+The function should return an array of integers which begins with a number
+represented by the startNum parameter and ends with a number represented by 
+the endNum parameter. The starting number will always be less than or equal 
+to the ending number. Your function must use recursion by calling itself and
+not use loops of any kind. It should also work for cases where both startNum
+and endNum are the same.
+*/ 
+
+
+
+function rangeOfNumbers(startNum, endNum) {
+  function recur(n){
+    if (n < startNum){
+      return []
+    } else{
+      var counter = recur(n-1)
+      counter.push(n)
+      return counter
+    }
+  }
+  return recur(endNum);
+};
