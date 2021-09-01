319f588514a3fb04ff13cfe89225268ceca2771b
Tsquare25 <tibebe1234t@gmail.com>
2021-09-01 15:30:34 +0100

    Problem: splice and slice

--- Tutorial-Solutions/Basic Algorithm Scripting.js
+++ Tutorial-Solutions/Basic Algorithm Scripting.js
@@ -121,7 +121,14 @@ function titleCase(str) {
   titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")
 
 
+// splice and slice 
 
+function frankenSplice(arr1, arr2, n){
+    let spliced = [...arr2]
+    spliced.splice(n, 0, ...arr1)
+    return spliced
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 
 
