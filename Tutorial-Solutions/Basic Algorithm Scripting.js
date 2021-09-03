aee246e96c39ee1c2417985063f4fc46b6436cf1
Tsquare25 <tibebe1234t@gmail.com>
2021-09-03 16:11:19 +0100

    New topic - Object Oriented Programming

--- Tutorial-Solutions/Basic Algorithm Scripting.js
+++ Tutorial-Solutions/Basic Algorithm Scripting.js
@@ -211,7 +211,28 @@ function chunkArrayInGroups(arr, size) {
 
 
 
+// Working with objects and prototypes
 
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+
+for(let property in beagle){
+  if(beagle.hasOwnProperty(property)){
+    ownProps.push(property)
+  }else{
+    prototypeProps.push(property)
+  }
+}
 
 
 
