b79e61f5080e799d1c7a9240238abf3029d1c7cd
Tsquare25 <tibebe1234t@gmail.com>
2021-09-05 23:19:11 +0100

    Apply Functional Programming to Convert Strings to URL Slugs

--- Functional Programming.js
+++ Functional Programming.js
@@ -108,3 +108,21 @@ function nonMutatingSort(arr) {
   // Only change code above this line
 }
 nonMutatingSort(globalArray);
+
+
+//Apply Functional Programming to Convert Strings to URL Slugs
+
+// the global variable
+var globalTitle = "Winter Is Coming";
+
+// Add your code below this line
+function urlSlug(title) {
+  return title
+    .trim()
+    .split(/\s+/)
+    .map(subWord => subWord.toLowerCase())
+    .join("-");
+}
+// Add your code above this line
+
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
