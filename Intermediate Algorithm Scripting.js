283199204c924fa3dd3295b8213a001f9d6b0d88
Tsquare25 <tibebe1234t@gmail.com>
2021-09-09 21:46:31 +0100

    Everything Be True

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -323,3 +323,16 @@ function binaryAgent(str){
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+
+//Everything Be True 
+
+function truthCheck(collection, pre) {
+  for (let i = 0 ; i < collection.length; i++){
+    if (collection[i][pre]) continue 
+    else return false
+  }
+  return true ;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
