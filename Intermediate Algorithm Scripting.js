dfa8ca2e57405197b5fafe7b379b4bfb41ad3514
Tsquare25 <tibebe1234t@gmail.com>
2021-09-09 21:41:12 +0100

    // From Binary to Alphabet

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -314,3 +314,12 @@ function steamrollArray(arr) {
 }
 
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+
+// From Binary to Alphabet 
+
+function binaryAgent(str){
+  return str.split(" ").map(element => String.fromCharCode(parseInt(element, 2))).join("")
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
