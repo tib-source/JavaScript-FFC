1e12ccbb2cef7bcb4c8733e37184b5bfd277ccfd
Tsquare25 <tibebe1234t@gmail.com>
2021-09-14 21:54:23 +0100

    Sum of odd numbers

--- Code Wars Solutions.js
+++ Code Wars Solutions.js
@@ -83,3 +83,42 @@ function digPow(n, p){
 }
 
 digPow(89, 1)
+
+
+//
+`
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+`
+function rowSumOddNumbers(n) {
+	// TODO
+  let start = n * (n - 1) + 1
+  let total = 0 
+  for(let i = 0 ; i < n; i++){
+    next = start + (2 *i)
+    total += next
+  }
+  return total
+}
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(rowSumOddNumbers(1), 1);
+    assert.strictEqual(rowSumOddNumbers(42), 74088);
+  });
+});
