1265d8ecff444df1fc5fe7945c24d584c225cb39
Tsquare25 <tibebe1234t@gmail.com>
2021-09-17 20:51:59 +0100

    Reverse Words - 7kyu

--- Code Wars Solutions.js
+++ Code Wars Solutions.js
@@ -175,3 +175,19 @@ function findOdd(A) {
   findOdd(A) 
   
   // expects a 5
+
+
+`Complete the function that accepts a string parameter,
+and reverses each word in the string.
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+`
+function reverseWords(str) {
+  // Go for it
+  return str.split(" ").map(word=> {
+    return word.split("").reverse().join("")
+  }).join(" ")
+}
