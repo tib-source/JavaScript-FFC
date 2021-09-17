bd68d5cb26bf2574d1a26569cd20b73725ec2261
Tsquare25 <tibebe1234t@gmail.com>
2021-09-17 20:42:50 +0100

    Find the odd int

--- Code Wars Solutions.js
+++ Code Wars Solutions.js
@@ -143,3 +143,35 @@ function isPangram(string){
   })
   return Object.keys(obj).length == 26 ? true : false   
   }
+
+
+`Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+`
+function findOdd(A) {
+  //happy coding!
+  let count = { }
+  for (let i =0 ; i < A.length; i++){
+    if (count.hasOwnProperty(A[i])) {
+      count[A[i]] += 1
+    }else{ count[A[i]] = 1 }                                      
+  }
+  console.log(count)
+  for(let value in count){
+    if (count[value] % 2 != 0) {return parseInt(value)}
+  }
+
+}
+  A = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
+  
+  findOdd(A) 
+  
+  // expects a 5
