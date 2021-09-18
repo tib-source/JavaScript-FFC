5db13a47c2d8f1a5873a2d2855a8477536d9f60c
Tsquare25 <tibebe1234t@gmail.com>
2021-09-18 11:09:02 +0100

    Two Sum - 6kyu

--- Code Wars Solutions.js
+++ Code Wars Solutions.js
@@ -238,3 +238,39 @@ let nums = {
   function minus(n) { return ` - ${n}`}
   function times(n) { return ` * ${n}`}
   function dividedBy(n) { return ` / ${n}`}
+
+
+`Write a function that takes an array of numbers (integers for the tests)
+ and a target number. It should find two different items in the array that
+,when added together, give the target value. The indices of these items
+should then be returned in a tuple like so: (index1, index2).
+For the purposes of this kata, some tests may have multiple answers;
+any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater,
+and all of the items will be numbers; target will always be the sum of two
+different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+
+twoSum [1, 2, 3] 4 === (0, 2)
+`
+
+function twoSum(numbers, target) {
+  // ...
+  let res = {}
+  for (let i = 0 ; i < numbers.length; i++) {
+    let curr = numbers[i]
+    let other = target - curr
+    if (res.hasOwnProperty(curr) && curr*2 == target){
+      return [res[curr], i]  
+    }else{
+      res[curr] = i
+      if (numbers.includes(other)){
+        if(other !== curr){
+        return [i, numbers.indexOf(other)]
+        }
+      }
+    }
+  }
+}
