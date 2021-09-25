5f2fbf8f64396b7c3d3647f6d8838cd611ab127d
Tsquare25 <tibebe1234t@gmail.com>
2021-09-25 19:59:39 +0100

    Finished the javascript calculations

--- Web-Developement/tip-calculator-app-main/app/js/index.js
+++ Web-Developement/tip-calculator-app-main/app/js/index.js
@@ -1,1 +1,63 @@
-console.log("Hello Word!")
+const bill = document.querySelector("#bill")
+const numPeople = document.querySelector("#numPeople")
+const tipAmmount = document.querySelector("#tipTotal")
+const total = document.querySelector("#total")
+const buttons = Array.from(document.querySelectorAll(".tip"))
+
+let initialbill
+let tipPercentage = 0
+let people = 1
+
+
+function turnActive(number,id){
+    const button = document.querySelector("#"+id)
+    if (button.classList.contains("active")){
+        return button.classList.remove("active")
+    }
+    buttons.forEach((button)=>{
+        button.classList.remove("active")
+    })
+    button.classList.add("active")
+    tipPercentage = parseInt(number)
+    tipCalculator()
+}
+
+function tipCalculator(){
+    if(people==0){people=1}
+    console.log(initialbill, people, tipPercentage)
+
+    let ammountToTip = initialbill  * (tipPercentage/100)
+    tipAmmount.textContent = (ammountToTip/people).toFixed(2)
+    total.textContent = ((initialbill/people)+ammountToTip/people).toFixed(2)
+}
+
+function customChange(field){
+    tipPercentage =  field.value
+    tipCalculator()
+}
+
+
+function num_people(field){
+    people =  field.value
+    tipCalculator()
+}
+
+function bill_change(field){
+    initialbill =  field.value
+    console.log(initialbill)
+    tipCalculator()
+}
+
+
+function reset(){
+    buttons.forEach((button)=>{
+        button.classList.remove("active")
+    })
+    initialbill = 0
+    people = 1
+    tipPercentage = 0 
+    bill.value = 0
+    numPeople.value = 0 
+    tipAmmount.textContent = (0).toFixed(2)
+    total.textContent = (0).toFixed(2)
+}
