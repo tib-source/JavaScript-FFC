b90aac9f872b6a594c16f712c68d3c0581109935
Tsquare25 <tibebe1234t@gmail.com>
2021-10-06 18:34:01 +0100

    did the fizzbuzz interview question

--- Algorithm Learning/Code Wars Solutions.js
+++ Algorithm Learning/Code Wars Solutions.js
@@ -490,3 +490,43 @@ function topThreeWords(text) {
 }
 
 topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") // ['e','ddd','aa']
+
+
+
+"------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+  `Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]`
+
+const fizzbuzz = (N) => {
+  let answer = []
+  for (let i = 1; i <= N; i++) {
+    if (i % 3 == 0) {
+      if (i % 5 == 0) {
+        answer.push('FizzBuzz')
+        continue
+      }
+      answer.push('Fizz')
+    }
+    else if (i % 5 == 0) {
+      answer.push('Buzz')
+    } else {
+      answer.push(i)
+    }
+  }
+  console.log(answer.join("-"))
+  return answer
+}
+
+
+
