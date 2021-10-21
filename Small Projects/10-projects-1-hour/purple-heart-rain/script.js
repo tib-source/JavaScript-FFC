64cf1649ba32c7323bd8cf5ca303e52525777a21
Tsquare25 <tibebe1234t@gmail.com>
2021-10-22 00:06:44 +0100

    element auto deletes after 4 seconds

--- Small Projects/10-projects-1-hour/purple-heart-rain/script.js
+++ Small Projects/10-projects-1-hour/purple-heart-rain/script.js
@@ -1,11 +1,13 @@
-const counter = 0
 
 function createPurple() {
   let purple = document.createElement('div');
   purple.style.left = (Math.random() * 100) + "vw";
   purple.innerHTML = "💜"
   document.body.appendChild(purple)
-  counter++
+
+  setTimeout(() => {
+    purple.remove()
+  }, 4000)
 }
 
 
