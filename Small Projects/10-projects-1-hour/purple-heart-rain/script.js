03c49a1d21bff3cf9128fa9efa8c16c984ed67e3
Tsquare25 <tibebe1234t@gmail.com>
2021-10-22 00:04:16 +0100

    added a fade away effect

--- Small Projects/10-projects-1-hour/purple-heart-rain/script.js
+++ Small Projects/10-projects-1-hour/purple-heart-rain/script.js
@@ -2,10 +2,11 @@ const counter = 0
 
 function createPurple() {
   let purple = document.createElement('div');
+  purple.style.left = (Math.random() * 100) + "vw";
   purple.innerHTML = "💜"
   document.body.appendChild(purple)
   counter++
 }
 
 
-setInterval(createPurple, 1000)
+setInterval(createPurple, 300)
